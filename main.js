bda2c488b94201f52005960b4e20593c515f479d
Dazzlesito <afcortesj@unadvirtual.edu.co>
2024-09-08 22:05:10 -0500

    automove slider added

--- main.js
+++ main.js
@@ -1,2 +1,38 @@
 import './style.css'
 
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlides);
+
+function initializeSlides() {
+  if (slides.length > 0) {
+    slides[slideIndex].classList.add("displaySlide");
+    setInterval(nextSlide, 5000);
+  }
+}
+
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  }
+
+  if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  slides.forEach(slide => {
+    slide.classList.remove("displaySlide")
+  });
+  slides[slideIndex].classList.add("displaySlide");
+}
+
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
+}
+
+function prevSlide() {
+
+}
